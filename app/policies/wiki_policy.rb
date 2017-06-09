023e1d86fcf3e6bb0f7c8f608a3d049ea04aa435
tedlopez <tedmlopez@yahoo.com>
2017-06-09 10:10:08 -0400

    Collaborators created

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -49,7 +49,7 @@ class WikiPolicy < ApplicationPolicy
         elsif user.role == 'premium'
           all_wikis = scope.all
           all_wikis.each do |wiki|
-            if !wiki.private? || wiki.user_id == user.id || wiki.collaborators.include?(user)
+            if !wiki.private? || wiki.user_id == user.id || wiki.users.include?(user)
               wikis << wiki # if the user is premium, only show them public wikis, or that private wikis they created, or private wikis they are a collaborator on
             end
         end
@@ -57,7 +57,7 @@ class WikiPolicy < ApplicationPolicy
         all_wikis = scope.all
         wikis = []
         all_wikis.each do |wiki|
-          if !wiki.private? || wiki.collaborators.include?(user)
+          if !wiki.private? || wiki.users.include?(user)
             wikis << wiki # only show standard users public wikis and private wikis they are a collaborator on
           end
         end
