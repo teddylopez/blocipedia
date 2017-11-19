a66267c537a403237eeffacf899c3c913f41eb36
tedlopez <tedmlopez@gmail.com>
2017-11-19 14:10:35 -0500

    Prod mailer environment edit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: 'https://fierce-mesa-78216.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
 
