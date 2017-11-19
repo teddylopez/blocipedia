ec32e1baea93ebfed36e51482a2ea1af7f27d072
tedlopez <tedmlopez@gmail.com>
2017-11-19 11:05:17 -0500

    Set up contact form

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'nokogiri'
 gem 'pygments.rb', '~> 0.6.3'
 gem 'will_paginate', '~> 3.1', '>= 3.1.6'
 gem 'will_paginate-bootstrap', '~> 1.0', '>= 1.0.1'
+gem 'mail_form', '~> 1.7'
 
 # Used for encrypting User passwords
 gem 'bcrypt'
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :wikis, :path => "notes"
+  resources :contacts, only: [:new, :create]
   resources :charges, only: [:new, :create]
   resources :wikis do
     resources :collaborators, only: [:new, :create, :destroy]
