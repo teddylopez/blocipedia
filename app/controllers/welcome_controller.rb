97bdddba5e99daea49e353c3ed82e7936ca6d071
teddylopez <tedmlopez@gmail.com>
2018-02-07 15:30:01 -0500

    More homepage mods.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,6 @@
 class WelcomeController < ApplicationController
   #before_filter :authenticate_user!, except: [ :home, :about ]
   def index
-
   end
 
   def about
@@ -9,7 +8,7 @@ class WelcomeController < ApplicationController
 
   def home
     if current_user
-      @my_wikis = Wiki.where(:user_id => current_user.id).order('updated_at DESC')
+      @my_wikis = Wiki.where(:user_id => current_user.id).all.limit(5).order('updated_at DESC')
     end
   end
 end
